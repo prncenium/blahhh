b870b0978486f20eb1200c0aa5aa0b20a97b5c84
prncenium <princecsk666@gmail.com>
2024-10-10 10:32:11 +0530

    buri buri

---
+++ code1.js
@@ -0,0 +1,9 @@
+// Declare two numbers
+const num1 = 10;
+const num2 = 20;
+
+// Add the numbers
+const sum = num1 + num2;
+
+// Print the result
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
